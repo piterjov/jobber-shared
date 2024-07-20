27b024cf01d4f23595d661cc98d494d6429aecf7
Petar <piterjov@gmail.com>
2024-07-20 18:01:07 +0200

    fix import buyer

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ export {
     IAuthResponse,
     IAuthUser,
 } from './interfaces/auth.interface';
-export { IBuyerDocument, IReduxBuyer } from './buyer.interface';
+export { IBuyerDocument, IReduxBuyer } from './interfaces/buyer.interface';
 export {
     IConversationDocument,
     IMessageDocument,
